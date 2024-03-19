0fd8bc8f6298c424c90abbbd5989d336811d9806
RohannXSingh <rohansinghhhhhh24@gmail.com>
2024-03-19 13:39:39 +0530

    BMI calculator

---
+++ script.js
@@ -0,0 +1,36 @@
+function calculate (){
+    const calculateHeight = parseFloat(document.getElementById('height').value);
+    const calculateWeight = parseFloat(document.getElementById('weight').value);
+    
+
+   if (isNaN(calculateHeight) || isNaN(calculateWeight) || calculateHeight <= 0 || calculateWeight <=0) {
+       document.getElementById('finalresult').innerText = "Enter valid Height or Weight";
+    //Check if calculatedHeight and calculatedWeight is valid or not
+   }
+       const BMIcalculate = calculateWeight / ((calculateHeight / 100) **2);
+       //(calculatedHeight / 100) converts heigt from cm to m
+       //(**2) is for meters square
+       let result;
+     if (BMIcalculate < 18.5) {
+        result = "You are Underweight"
+     }else if (BMIcalculate < 25) {
+        result = "Your weight is normal"
+     }else if (BMIcalculate < 30){
+        result = "You are over weight"
+     }else{
+        result = "Go to gym"
+     }
+  const finalValue = `BMI is : ${BMIcalculate.toFixed(2)}, ${result}`;
+  const declaredResult = document.getElementById('finalresult').innerText = finalValue;
+}
+
+
+
+
+
+
+
+
+
+
+
